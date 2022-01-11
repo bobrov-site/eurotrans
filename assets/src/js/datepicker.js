fb730318da705144a7eeab2cb661e3ce68d8ab78
Daniil Bobrov <bobdaniil@yandex.ru>
2022-01-11 14:20:27 +0300

    datepicker installed and setup start day

---
+++ assets/src/js/datepicker.js
@@ -0,0 +1,26 @@
+//= ../../../node_modules/js-datepicker/dist/datepicker.min.js
+
+const pickerStart = datepicker('.dateStart',
+    {
+        id : 1,
+        customDays : ['Вс', 'Пн' , 'Вт' , 'Ср' , 'Чт' , 'Пт' , 'Сб'],
+        customMonths : ['Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль', 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь', 'Декабрь'],
+        customOverlayMonths: ['Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль', 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь', 'Декабрь'],
+        formatter : (input, date, instance) => {
+            const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+            const value = date.toLocaleDateString('ru-RU', options);
+            input.value = value
+        }
+    }
+);
+const pickerEnd = datepicker('.dateEnd',
+    {
+        id : 1,
+        customDays : ['Вс', 'Пн' , 'Вт' , 'Ср' , 'Чт' , 'Пт' , 'Сб'],
+        customMonths : ['Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль', 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь', 'Декабрь'],
+        customOverlayMonths: ['Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль', 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь', 'Декабрь']
+    }
+);
+
+pickerStart.getRange();
+pickerEnd.getRange();
